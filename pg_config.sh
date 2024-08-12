55d3aaeb709ec6577dba07c91d5af6f508eb6770
Amankhojaev Nodirkhon <44620146+n0di@users.noreply.github.com>
2024-08-12 17:10:16 +0500

    Update pg_config.sh

--- pg_config.sh
+++ pg_config.sh
@@ -1,3 +1,9 @@
 # show config files postgres
 sudo -u postgres psql -c "show config_file;"
 
+#======================= edit pg_hba.conf ==================================
+# add to end file 
+# host    all             new_pg_user      0.0.0.0/32              md5
+
+#======================= edit postgresql.conf ==============================
+
